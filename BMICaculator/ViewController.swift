7efc9e0168d55b7e974d87b6b563dd17e4f0f713
Neo <neo.chuang96@gmail.com>
2025-05-24 11:00:55 +0800

    Initial Commit

---
+++ BMICaculator/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BMICaculator
+//
+//  Created by Neo on 2025/5/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
