1d49c473be4827b34fa4fc6ca938d6eaee0eb1d0
Neo <neo.chuang96@gmail.com>
2025-07-26 22:03:46 +0800

    extension

--- BMICaculator/extension.swift
+++ BMICaculator/extension.swift
@@ -6,3 +6,10 @@
 //
 
 import Foundation
+
+extension Double {
+    func rounding(toDecimal decimal: Int) -> Double {
+        let numberOfDigits = pow(10.0, Double(decimal))
+        return (self * numberOfDigits).rounded(.toNearestOrAwayFromZero) / numberOfDigits
+    }
+}
